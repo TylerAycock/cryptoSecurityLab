5dc6ff0036db7d97aa8106aff65aa85aec45758a
Tyler Aycock <tyler.joseph.aycock@gmail.com>
2023-06-08 12:45:46 -0700

    completed lab step 1-2

---
+++ server/controller.js
@@ -0,0 +1,36 @@
+const bcrypt = require(`bcryptjs`)
+
+const users = []
+
+module.exports = {
+    login: (req, res) => {
+      console.log('Logging In User')
+      console.log(req.body)
+      const { username, password } = req.body
+      for (let i = 0; i < users.length; i++) {
+        if (users[i].username === username && bcrypt.compareSync(password,users[i].password)) {
+          let userCopy = {...users[i]}
+          delete userCopy.password
+          res.status(200).send(userCopy)
+          return
+        }
+      }
+      res.status(400).send("User not found.")
+    },
+    register: (req, res) => {
+        console.log('Registering User')
+        console.log(req.body)
+        let {password} = req.body
+        let salt = bcrypt.genSaltSync(8)
+        console.log(`The users password: ${password}`)
+        let hashedCode = bcrypt.hashSync(password,salt)
+        console.log(hashedCode)
+        password = hashedCode
+        users.push(password)
+        console.log(`the users array now contains:${users} `)
+        let userCopy = {...req.body}
+        delete userCopy.password
+        
+        res.status(200).send(req.body)
+    }
+}
